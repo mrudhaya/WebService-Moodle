9fa86a6dbce08e2b5d26a330b7d0d56a88660a79
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2011-12-19 15:21:19 -0200

    Adicionado arquivo de idiomas. Nome do Plugin adicionado.

--- local/sistemaaulaws/version.php
+++ local/sistemaaulaws/version.php
@@ -1,5 +1,5 @@
 <?php
 
-$plugin->version = 2011121900;
+$plugin->version = 2011121900.02;
 $plugin->requires = 2011033005.00;    // YYYYMMDDHH (Versão Obrigatoria do Moodle 2.0.5+)
 $plugin->maturity = MATURITY_ALPHA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE (Moodle 2.0 and above)
