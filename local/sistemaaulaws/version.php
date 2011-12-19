a4294f0345e12cd29318c9bcf14958169291acba
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2011-12-19 15:12:20 -0200

    Atualizada a versão do componente e alterado seu nível de maturidade para ALPHA.

--- local/sistemaaulaws/version.php
+++ local/sistemaaulaws/version.php
@@ -1,3 +1,5 @@
 <?php
 
-$plugin->version = 2009101900;
+$plugin->version = 2011121900;
+$plugin->requires = 2011033005.00;    // YYYYMMDDHH (Versão Obrigatoria do Moodle 2.0.5+)
+$plugin->maturity = MATURITY_ALPHA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE (Moodle 2.0 and above)
