6a2f70a320fae24e08b450fb88c78bfef0da1b81
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2011-12-19 15:57:26 -0200

    Implementado as configurações do banco de dados para instalação. Agora o WebService do Aula ao ser instalado automáticamente insere os dados das novas classes e funções externas.

---
+++ local/sistemaaulaws/db/services.php
@@ -0,0 +1,21 @@
+<?php
+
+$functions = array(
+    'sistemaaula_grade_get_final_grade_by_user_id_and_course_id' => array(           //web service name (unique in all Moodle)
+        'classname'		=> 'sistemaaula_grade_external', //class containing the function implementation
+        'methodname'	=> 'get_final_grade_by_user_id_and_course_id',              //name of the function into the class
+        'classpath'		=> 'local/sistemaaulaws/grade/externallib.php',     //file containing the class (only used for core external function, not needed if your file is 'component/externallib.php'),
+        'description'	=> 'Retorna a grade final do aluno conforme o curso informado.',
+        'capabilities'	=> 'moodle/grade:export, gradeexport/txt:view',
+        'type'			=> 'read'
+)
+);
+
+$services = array(
+    'servicename' => array(
+        'functions' => array ('sistemaaula_grade_get_final_grade_by_user_id_and_course_id'), //web service function name
+        'requiredcapability' => 'moodle/grade:export, gradeexport/txt:view',                  
+        'restrictedusers' => 1,
+        'enabled'=>0, //used only when installing the services
+)
+);
